a26c5c6fd7a7f11fd4ae4754dbb978eaaad85147
raj <rjha9223@gmail.com>
2026-06-14 14:41:13 +0530

    opa

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web" {
+
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.public.id
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  tags = {
+    Name        = "ai-governance-server"
+    Environment = "dev"
+    Owner       = "devops"
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+aws_region = "us-east-1"
+ami_id = "ami-0521cb2d60cfbb1a6"
